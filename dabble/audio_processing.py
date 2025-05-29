83f3be3ccc5162200fe166a74660788e47206390
lovemonkey257 <lovemonkey257@digital-gangsters.org>
2025-05-29 21:27:46 +0100

    Refactor

--- dabble/audio_processing.py
+++ dabble/audio_processing.py
@@ -10,24 +10,34 @@ import alsaaudio
 
 class AudioProcessing():
 
-    def __init__(self):
+    def __init__(self, sample_rate:int=44100, frame_chunk_size:int=340):
         self.max_value=2**16
+        
         self.p=pyaudio.PyAudio()
-        self.stream=None
+        self.stream:pyaudio.Stream = None
+        self.sample_rate = sample_rate
+        self.frames_chunk_size = frame_chunk_size # 160*4 #512
+
         self.mixer = alsaaudio.Mixer()
-        self.volume=50 #self.mixer.getvolume()
+        self.volume=50 
+        self.ch_l = None
+        self.ch_r = None
+        self.signal = None
+        
 
     def vol_up(self, vinc:int=1):
         self.volume+=vinc
         if self.volume>100:
             self.volume=100
         self.mixer.setvolume(self.volume)
+        return self.volume        
 
     def vol_down(self, vinc:int=1):
         self.volume-=vinc
         if self.volume<0:
             self.volume=0
         self.mixer.setvolume(self.volume)
+        return self.volume
     
     def set_volume(self, vol:int):
         if vol>=0 and vol<=100:
@@ -37,22 +47,20 @@ class AudioProcessing():
     def start(self):
         self.stream=self.p.open(format=pyaudio.paInt16,
                     channels=2,
-                    rate=44100,
+                    rate=self.sample_rate,
                     input=True,
                     start=False, # Need to wait for dablin to catch up
-                    frames_per_buffer=1024)
+                    frames_per_buffer=self.frames_chunk_size)
         return self.stream
 
     def get_peaks(self):
         if self.stream is None:
             return (0,0)
         
-        d=self.stream.read(1024,exception_on_overflow=False)
-        wav=np.frombuffer(d,dtype='int16')
-        ch_l=wav[0::2]
-        ch_r=wav[1::2]
-        # peak_l = np.abs(np.max(ch_l)-np.min(ch_l))/self.max_value*100
-        peak_l = np.abs(np.max(ch_l))/self.max_value*100
-        #peak_rR = np.abs(np.max(ch_r)-np.min(ch_r))/self.max_value*100
-        peak_r = np.abs(np.max(ch_r))/self.max_value*100        
+        d=self.stream.read(self.frames_chunk_size, exception_on_overflow=False)
+        self.signal=np.frombuffer(d,dtype='int16')
+        self.ch_l=self.signal[0::2]
+        self.ch_r=self.signal[1::2]
+        peak_l = np.abs(np.max(self.ch_l))/self.max_value*100
+        peak_r = np.abs(np.max(self.ch_r))/self.max_value*100        
         return (peak_l, peak_r)
