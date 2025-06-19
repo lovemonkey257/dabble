258f1ebad54f1f79e2e8e71e35b31d6d0e8c49a1
Andy <lovemonkey257@digital-gangsters.org>
2025-06-19 17:20:43 +0100

    Sort imports. Only calc levels if needed

--- dabble/audio_processing.py
+++ dabble/audio_processing.py
@@ -4,14 +4,15 @@ Need: apt-get install python3-alsaaudio
 Volume code: https://askubuntu.com/questions/689521/control-volume-using-python-script
 '''
 
-import pyaudio
-import numpy as np
 import alsaaudio
+import numpy as np
+import pyaudio
+
 
 class AudioProcessing():
 
     def __init__(self, sample_rate:int=44100, frame_chunk_size:int=340):
-        self.max_value=2**16
+        self._max_value=2**16
         
         self.p=pyaudio.PyAudio()
         self.stream:pyaudio.Stream = None
@@ -45,22 +46,33 @@ class AudioProcessing():
             self.mixer.setvolume(self.volume)
 
     def start(self):
-        self.stream=self.p.open(format=pyaudio.paInt16,
-                    channels=2,
-                    rate=self.sample_rate,
-                    input=True,
-                    start=False, # Need to wait for dablin to catch up
-                    frames_per_buffer=self.frames_chunk_size)
+        self.stream=self.p.open(
+                        format=pyaudio.paInt16,
+                        channels=2,
+                        rate=self.sample_rate,
+                        input=True,
+                        start=False, # Need to wait for dablin to catch up
+                        frames_per_buffer=self.frames_chunk_size
+        )
         return self.stream
 
-    def get_peaks(self):
+    def get_sample(self) -> bool:
+        '''
+        Get live sample. Updates self.signal with stereo data
+        returns:
+            True: sample updated
+            False: no sound available
+        '''
         if self.stream is None:
-            return (0,0)
+            return False
         
         d=self.stream.read(self.frames_chunk_size, exception_on_overflow=False)
         self.signal=np.frombuffer(d,dtype='int16')
+        return True
+
+    def get_peaks(self) -> tuple[float,float]:
         self.ch_l=self.signal[0::2]
         self.ch_r=self.signal[1::2]
-        peak_l = np.abs(np.max(self.ch_l))/self.max_value*100
-        peak_r = np.abs(np.max(self.ch_r))/self.max_value*100        
+        peak_l = np.abs(np.max(self.ch_l))/self._max_value*100.0
+        peak_r = np.abs(np.max(self.ch_r))/self._max_value*100.0        
         return (peak_l, peak_r)
