2c9f2928b50ef0cb48076c85938b9e0767311a26
Andy <lovemonkey257@digital-gangsters.org>
2025-07-13 20:36:43 +0100

    Consistent volume setting & logging

--- dabble/audio_processing.py
+++ dabble/audio_processing.py
@@ -4,10 +4,12 @@ Need: apt-get install python3-alsaaudio
 Volume code: https://askubuntu.com/questions/689521/control-volume-using-python-script
 '''
 
+import logging
 import alsaaudio
 import numpy as np
 import pyaudio
 
+logger = logging.getLogger(__name__)
 
 class AudioProcessing():
 
@@ -20,30 +22,36 @@ class AudioProcessing():
         self.frames_chunk_size = frame_chunk_size # 160*4 #512
 
         self.mixer = alsaaudio.Mixer()
-        self.volume=50 
+        self.volume=2
         self.ch_l = None
         self.ch_r = None
         self.signal = None
+        self.channel = alsaaudio.MIXER_CHANNEL_ALL
+        self.set_volume(self.volume)
+        logger.info("Volume set to %d", self.volume)
         
 
-    def vol_up(self, vinc:int=1):
+    def vol_up(self, vinc:int=2):
         self.volume+=vinc
         if self.volume>100:
             self.volume=100
-        self.mixer.setvolume(self.volume)
+        self.set_volume(self.volume)
         return self.volume        
 
-    def vol_down(self, vinc:int=1):
+    def vol_down(self, vinc:int=2):
         self.volume-=vinc
         if self.volume<0:
             self.volume=0
-        self.mixer.setvolume(self.volume)
+        self.set_volume(self.volume)
         return self.volume
     
     def set_volume(self, vol:int):
-        if vol>=0 and vol<=100:
-            self.volume = vol
-            self.mixer.setvolume(self.volume)
+        if vol<0:
+            vol=0
+        elif vol>80:
+            vol=80
+        self.volume = vol
+        self.mixer.setvolume(vol, self.channel)
 
     def start(self):
         self.stream=self.p.open(
@@ -75,4 +83,5 @@ class AudioProcessing():
         self.ch_r=self.signal[1::2]
         peak_l = np.abs(np.max(self.ch_l))/self._max_value*100.0
         peak_r = np.abs(np.max(self.ch_r))/self._max_value*100.0        
+        logger.debug("Peaks L:%d R:%d", peak_l, peak_r)
         return (peak_l, peak_r)
