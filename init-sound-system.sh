72eb139379a402c427515f5e3c6da4b724a0f55a
Andy <lovemonkey257@digital-gangsters.org>
2025-08-26 17:14:51 +0100

    Use first sink rather than hardcode it

--- init-sound-system.sh
+++ init-sound-system.sh
@@ -2,7 +2,7 @@
 #
 #
 sudo modprobe snd-aloop pcm_substreams=1
-SRC=alsa_output.platform-soc_107c000000_sound.stereo-fallback.monitor
+SRC=$(pactl list sinks short | head -1 | cut -f2)
 SINK=alsa_output.platform-snd_aloop.0.analog-stereo
 pactl load-module module-loopback source=$SRC sink=$SINK
 
