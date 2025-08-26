86a890e3ed4335a0eb4540b97be7bdb02eb6478e
Andy <lovemonkey257@digital-gangsters.org>
2025-08-26 18:42:50 +0100

    Doh. Use sources not sinks

--- init-sound-system.sh
+++ init-sound-system.sh
@@ -2,7 +2,7 @@
 #
 #
 sudo modprobe snd-aloop pcm_substreams=1
-SRC=$(pactl list sinks short | head -1 | cut -f2)
+SRC=$(pactl list sources short | head -1 | cut -f2)
 SINK=alsa_output.platform-snd_aloop.0.analog-stereo
 pactl load-module module-loopback source=$SRC sink=$SINK
 
