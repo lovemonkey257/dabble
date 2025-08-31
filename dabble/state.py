8732eda210481d383105bf76c4b6e4b77c91881f
Andy <lovemonkey257@digital-gangsters.org>
2025-08-31 16:14:07 +0100

    Moved state utils into own module

---
+++ dabble/state.py
@@ -0,0 +1,43 @@
+import json
+import logging
+from pathlib import Path
+from dabble import (lcd_ui)
+
+logger = logging.getLogger(__name__)
+config_path = Path("dabble_radio.json")
+
+def load_state(state:lcd_ui.UIState):
+    logger.info(f'Loading saved state from {config_path}')
+    config=dict()
+    if config_path.exists():
+        with open(config_path, "r") as f:
+            config = json.load(f)
+            state.volume = config['volume']
+            state.station_name = config['station_name']
+            state.ensemble = config['ensemble']
+            state.visualiser_enabled = config['enable_visualiser']
+            state.visualiser = config['visualiser']
+            state.levels_enabled = config['enable_levels']
+            state.pulse_left_led_encoder = config['pulse_left_led_encoder']
+            state.pulse_right_led_encoder = config['pulse_right_led_encoder']
+            state.station_enabled = config['station_enabled'] if 'station_enabled' in config else True
+    else:
+        state.station_name = "Magic Radio"
+    return config
+
+def save_state(state:lcd_ui.UIState):
+    logger.info("Saving state")
+    config = {
+        "station_name": state.station_name,
+        "ensemble": state.ensemble,
+        "volume": state.volume,
+        "pulse_left_led_encoder": state.pulse_left_led_encoder,
+        "pulse_right_led_encoder": state.pulse_right_led_encoder,
+        "enable_visualiser": state.visualiser_enabled,
+        "visualiser": state.visualiser,
+        "enable_levels": state.levels_enabled,
+        "station_enabled": state.station_enabled
+    }
+    with open(config_path, "w") as f:
+        f.write(json.dumps(config))
+
