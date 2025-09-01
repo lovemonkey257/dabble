9e47263aad6f892e22214b4b4eeaacf54aac8614
lovemonkey <lovemonkey257@digital-gangsters.org>
2025-09-01 09:43:23 +0100

    WIP

--- init-sound-system.sh
+++ init-sound-system.sh
@@ -5,5 +5,5 @@ sudo modprobe snd-aloop pcm_substreams=1
 SRC=$(pactl list sources short | head -1 | cut -f2)
 SINK=alsa_output.platform-snd_aloop.0.analog-stereo
 echo pactl load-module module-loopback source=$SRC sink=$SINK
-pactl load-module module-loopback source=$SRC sink=$SINK
+pactl load-module module-loopback source=$SRC sink=$SINK channels=2
 
