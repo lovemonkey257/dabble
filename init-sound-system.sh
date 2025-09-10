122fb74e808f9fc825baca04f9edb2e781dec9c9
Andy <lovemonkey257@digital-gangsters.org>
2025-09-10 10:07:33 +0100

    Add message to say not always necessary

--- init-sound-system.sh
+++ init-sound-system.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 #
 #
+echo "Only run if no record interface on sound card. Will use first sound card so"
+echo "you may need to tweak this or disable unneeded soundcards e.g. HDMI on Pi"
+echo "Ctrl+C if you dont want to continue, otherwise press return"
+read x
 sudo modprobe snd-aloop pcm_substreams=1
 SRC=$(pactl list sources short | head -1 | cut -f2)
 SINK=alsa_output.platform-snd_aloop.0.analog-stereo
